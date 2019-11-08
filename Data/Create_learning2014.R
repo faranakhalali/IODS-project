302fa88beb5992ac719a4fde5b68b0af12452985
Faranak Halali <faranah@uef.fi>
2019-11-08 20:03:20 +0200

    Data wrangling (combining, scaling, excluding) Regression, plot matrices, model summary, model validation by three plots

--- Data/Create_learning2014.R
+++ Data/Create_learning2014.R
@@ -1,2 +1,2 @@
 # Faranak Halali, 07.11.2019, Second week of Open Data Science: Regression
-# This data has 183 observations for 60 variables. The variables inlcude several questions about different topics along with Age, combination score for Attitude, gender and total points.
+
