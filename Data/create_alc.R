66c684d1c65bd52e418925a6ec768686bef22a5c
faranakhalali <faranak.halali@uef.fi>
2019-11-18 17:15:39 +0200

    Final Script

--- Data/create_alc.R
+++ Data/create_alc.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Faranak Halali
 #Exercise 3 - week 3
 #Logistic Regression - 15.11.2019 
@@ -65,73 +64,3 @@ glimpse(alc)
 #everything looks right! It has 382 observations with 35 variables
 #save the data
 write.csv(alc, "~/IODS-project/Data/alc.csv")
-
-=======
-#Faranak Halali
-#Exercise 3 - week 3
-#Logistic Regression - 15.11.2019 
-# R script for data wrangling, data source:https://archive.ics.uci.edu/ml/datasets/Student+Performance
-
-#reading data 
-student.mat <- read.csv("~/IODS-project/Data/student-mat.csv", header=TRUE, sep=";")
-student.por <- read.csv("~/IODS-project/Data/student-por.csv", header=TRUE, sep=";")
-
-
-#exploring data
-str(student.mat)
-dim(student.mat)
-str(student.por)
-dim(student.por)
-
-# access the dplyr library
-library(dplyr)
-# common columns to use as identifiers
-join_by <- c("school", "sex", "age", "address", "famsize", "Pstatus", "Medu", "Fedu", "Mjob", "Fjob", "reason", "nursery","internet")
-# join the two datasets by the selected identifiers
-math_por <- inner_join(student.mat, student.por, by = join_by, suffix = c(".math", ".por"))
-
-#explore new data
-str(math_por)
-dim(math_por)
-
-
-# create a new data frame with only the joined columns
-alc <- select(math_por, one_of(join_by))
-
-# columns that were not used for joining the data
-notjoined_columns <- colnames(student.mat)[!colnames(student.mat) %in% join_by]
-
-# print out the columns not used for joining
-notjoined_columns
-
-# for every column name not used for joining...
-for(column_name in notjoined_columns) {
-  # select two columns from 'math_por' with the same original name
-  two_columns <- select(math_por, starts_with(column_name))
-  # select the first column vector of those two columns
-  first_column <- select(two_columns, 1)[[1]]
-  
-  # if that first column  vector is numeric...
-  if(is.numeric(first_column)) {
-    # take a rounded average of each row of the two columns and
-    # add the resulting vector to the alc data frame
-    alc[column_name] <- round(rowMeans(two_columns))
-  } else { # else if it's not numeric...
-    # add the first column vector to the alc data frame
-    alc[column_name] <- first_column
-  }
-}
-
-# define a new column alc_use by combining weekday and weekend alcohol use
-alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
-
-# define a new logical column 'high_use'
-alc <- mutate(alc, high_use = alc_use > 2)
-
-#look at data to see if everything is in order
-glimpse(alc)
-
-#save the data
-write.csv(alc, "~/IODS-project/Data/alc.csv")
-
->>>>>>> fb86759018513413c9878b513f9335c1d0ea3507
