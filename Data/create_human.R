4cf0fdcb0b09ef6e529954b7c3c1be3b4d5471fb
faranakhalali <faranak.halali@uef.fi>
2019-12-01 19:20:51 +0200

    5

--- Data/create_human.R
+++ Data/create_human.R
@@ -1,5 +1,6 @@
 # Faranak Halali
 # Week 4, Clustering and classification, 23.11.2019
+# Data source:http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt
 
 # Reading data
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
@@ -47,5 +48,39 @@ write.csv(human, "~/IODS-project/Data/human.csv")
 
 
 
+# Continue data wrangling for dimensionality reduction techniques week
+# Read data
+human <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt", header=TRUE, sep=",")
+str(human)
+dim(human)
+# human data has 195 observations and 19 variables. It combines several indicators from most countries in the world. It contains variables related to health and knowledge, e.g., Gross National Income per capita and Expected years of schooling. Also, it includes variables related to empowerment, e.g., Percetange of female representatives in parliament and Proportion of females in the labour force. 
 
+# Mutate the data
+library(stringr)
+library(dplyr)
+str(human$GNI)
+GNI_ <- str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+human <- mutate(human, GNI_1 = GNI_)
+
+# Exclude unneeded variables
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- select(human, one_of(keep))
+
+# Remove all rows with missing values 
+complete.cases(human)
+data.frame(human[-1], comp = complete.cases(human))
+human_ <- filter(human, complete.cases(human))
+
+# Define the row names by the country
+last <- nrow(human_) - 7
+human_ <- human_[1:last, ]
+rownames(human_) <- human_$Country
+
+# Drop Country column
+human_ <- subset(human_, select = -c(Country))
+# Looks right. 155 observations with 8 variables
+
+# Save the data
+
+write.csv(human_, "~/IODS-project/Data/human.csv")
               
